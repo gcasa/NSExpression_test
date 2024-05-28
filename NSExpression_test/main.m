a273772bd7cdf15f9d47413b3287cd41937d905c
Gregory Casamento <greg.casamento@gmail.com>
2024-05-28 12:43:00 -0400

    Add code to test new methods

--- NSExpression_test/main.m
+++ NSExpression_test/main.m
@@ -14,6 +14,26 @@ int main(int argc, const char * argv[]) {
         
         NSExpression *expression2 = [NSExpression expressionWithFormat: @"%f*%f" argumentArray:@[@3.4,@3.1]];
         NSLog(@"%@",[expression2 expressionValueWithObject:nil context:nil]);
+        
+        NSExpression *expression3 = [NSExpression expressionForAggregate:[NSArray arrayWithObjects: expression, expression2, nil]];
+        NSLog(@"%@",[expression3 expressionValueWithObject:nil context:nil]);
+        
+        NSExpression *set1 = [NSExpression expressionForAggregate: [NSArray arrayWithObjects:
+                                                                    [NSExpression expressionForConstantValue: @"A"],
+                                                                    [NSExpression expressionForConstantValue: @"B"],
+                                                                    [NSExpression expressionForConstantValue: @"C"], nil]];
+        NSExpression *set2 = [NSExpression expressionForAggregate: [NSArray arrayWithObjects:
+                                                                    [NSExpression expressionForConstantValue: @"C"],
+                                                                    [NSExpression expressionForConstantValue: @"D"],
+                                                                    [NSExpression expressionForConstantValue: @"E"], nil]];
+        NSExpression *expression4 = [NSExpression expressionForIntersectSet:set1 with:set2];
+        NSLog(@"%@",[expression4 expressionValueWithObject:nil context:nil]);
+        
+        NSExpression *expression5 = [NSExpression expressionForUnionSet:set1 with:set2];
+        NSLog(@"%@",[expression5 expressionValueWithObject:nil context:nil]);
+        
+        NSExpression *expression6 = [NSExpression expressionForMinusSet:set1 with:set2];
+        NSLog(@"%@",[expression6 expressionValueWithObject:nil context:nil]);
     }
     return 0;
 }
